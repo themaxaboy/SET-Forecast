64ff5e67607ba74e2a381dbf6d25102f58665872
themaxaboy <themaxaboy@hotmail.com>
2017-11-03 00:31:11 +0700

    move

--- main.go
+++ main.go
@@ -1,67 +1,25 @@
 package main
 
 import (
-	"fmt"
-	//"math/rand"
-	"encoding/csv"
 	"bufio"
-    "os"
+	"encoding/csv"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
+	"math"
+	"math/rand"
+	"os"
 	"strconv"
 
-	//"github.com/goml/gobrain"
+	"github.com/goml/gobrain"
 )
 
-var setIndex = make([]float64,0)
-var setIndexPercent = make([]float64,0)
-var patterns = make([][][]float64,0)
+var setIndex = make([]float64, 0)
+var setIndexPercent = make([]float64, 0)
+var patterns = make([][][]float64, 0)
 
-func readFile(filePath string) {
-    // Load a TXT file.
-    f, _ := os.Open(filePath)
-
-    // Create a new reader.
-    r := csv.NewReader(bufio.NewReader(f))
-    for {
-        record, err := r.Read()
-        // Stop at EOF.
-        if err == io.EOF {
-            break
-        }
-
-        //fmt.Println(record)
-		if (record[0] == "SET") {
-			//fmt.Printf("%v %v %v\n", record[0],record[1],record[5])
-			v := record[5]
-			if s, err := strconv.ParseFloat(v, 64); err == nil {
-				//fmt.Printf("%v\n", s)
-				setIndex = append(setIndex, s)
-			}
-			break
-		}
-    }
-}
-
-func convertToPatterns () {
-	for i := 5; i < len(setIndexPercent); i++ {
-		patterns = append(patterns, ({0, 1, 2, 3, 4}, {5}) )
-	}
-}
-
-func convertToPercent () {
-	for i := 0; i < len(setIndex); i++ {
-		if (i == 0) {
-			setIndexPercent = append(setIndexPercent, 0)
-		} else {
-		cal := (1 - (setIndex[i-1] / setIndex[i])) * 100
-		setIndexPercent = append(setIndexPercent, cal)
-		}
-	}
-}
-
-func readDir(path string){
+func readDir(path string) {
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
 		log.Fatal(err)
@@ -73,28 +31,74 @@ func readDir(path string){
 	}
 }
 
+func readFile(filePath string) {
+	// Load a TXT file.
+	f, _ := os.Open(filePath)
+
+	// Create a new reader.
+	r := csv.NewReader(bufio.NewReader(f))
+	for {
+		record, err := r.Read()
+		// Stop at EOF.
+		if err == io.EOF {
+			break
+		}
+
+		//fmt.Println(record)
+		if record[0] == "SET" {
+			//fmt.Printf("%v %v %v\n", record[0],record[1],record[5])
+			v := record[5]
+			if s, err := strconv.ParseFloat(v, 64); err == nil {
+				//fmt.Printf("%v\n", s)
+				setIndex = append(setIndex, s)
+			}
+			break
+		}
+	}
+}
+
+func convertToPatterns() {
+	for i := 5; i < len(setIndexPercent); i++ {
+		dataSet := [][]float64{[]float64{setIndexPercent[i-5], setIndexPercent[i-4], setIndexPercent[i-3], setIndexPercent[i-2], setIndexPercent[i-1]}, {setIndexPercent[i]}}
+		patterns = append(patterns, dataSet)
+	}
+}
+
+func convertToPercent() {
+	for i := 0; i < len(setIndex); i++ {
+		if i == 0 {
+			setIndexPercent = append(setIndexPercent, 0)
+		} else {
+			cal := ((setIndex[i] - setIndex[i-1]) / setIndex[i-1]) * 100
+			cal = toFixed(cal, 2)
+			setIndexPercent = append(setIndexPercent, cal)
+		}
+	}
+}
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
+
 func main() {
-	//readDir("./set-history_EOD/1975-2016/")
-	readDir("./set-history_EOD/2016-2017/")
+	//readDir("./set-history_EOD/1975-1999/")
+	readDir("./set-history_EOD/2000-2016/")
+	readDir("./set-history_EOD/2017-2018/")
 	//fmt.Printf("%v\n", setIndex)
 
 	convertToPercent()
-	fmt.Printf("%v\n", setIndexPercent)
-	
-	convertToPatterns()
-	fmt.Printfln("%v\n", patterns)
-	
-	/*// set the random seed to 0
-	rand.Seed(0)
+	//fmt.Printf("%v\n", setIndexPercent)
 
-	// create the XOR representation patter to train the network
-	patterns := [][][]float64{
-		{{0, 1, 2, 3, 4}, {5}},
-		{{0, 1, -1, 1, -1}, {0}},
-		{{0, 5, 4, 3, 2}, {-2}},
-		{{0, -2, -3, -4, -5}, {-8}},
-		{{0, -8, -6, -2, -3}, {2}},
-	}
+	convertToPatterns()
+	//fmt.Printf("%v\n", patterns)
+
+	// set the random seed to 0
+	rand.Seed(0)
 
 	// instantiate the Feed Forward
 	ff := &gobrain.FeedForward{}
@@ -108,13 +112,19 @@ func main() {
 	// the training will run for 1000 epochs
 	// the learning rate is set to 0.6 and the momentum factor to 0.4
 	// use true in the last parameter to receive reports about the learning error
-	ff.Train(patterns, 10000, 0.6, 0.4, false)
+	ff.Train(patterns, 1000, 0.6, 0.4, false)
 
 	// testing the network
+	/*store = */
 	ff.Test(patterns)
+	/*for _, p := range store {
+		fmt.Printf("%v\n", p)
+	}*/
 
 	// predicting a value
-	inputs := []float64{0, -1, 2, -2, 1}
+	//inputs := []float64{0.42075325, 0.15326072, 0.15768098, -0.39619605, -0.73605319}
+	inputs := []float64{0.42, 0.15, 0.16, -0.40, -0.74}
 	output := ff.Update(inputs)
-	fmt.Printf("%g\n", output)*/
+
+	fmt.Printf("\n%g\n", output)
 }
